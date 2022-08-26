4daf61add17d6650c951ee4492b7ecb31c20919f
Kei Kamikawa <code-hex@users.noreply.github.com>
2022-08-26 11:02:44 +0900

    Initial Commit

---
+++ vm-scroll-test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  vm-scroll-test
+//
+//  Created by Kei Kamikawa on 2022/08/26.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
